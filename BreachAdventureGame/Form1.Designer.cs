cdddca7926f521bad8b9ab2ee698ee8c2978f7b8
Miguel Montoya <migumont644@ed.amdsb.ca>
2021-02-24 14:42:19 -0500

    Add project files.

---
+++ BreachAdventureGame/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BreachAdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.sceneImage = new System.Windows.Forms.PictureBox();
+            this.redImage = new System.Windows.Forms.PictureBox();
+            this.blueImage = new System.Windows.Forms.PictureBox();
+            this.greenImage = new System.Windows.Forms.PictureBox();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(776, 74);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // sceneImage
+            // 
+            this.sceneImage.Location = new System.Drawing.Point(15, 86);
+            this.sceneImage.Name = "sceneImage";
+            this.sceneImage.Size = new System.Drawing.Size(773, 239);
+            this.sceneImage.TabIndex = 1;
+            this.sceneImage.TabStop = false;
+            // 
+            // redImage
+            // 
+            this.redImage.Image = ((System.Drawing.Image)(resources.GetObject("redImage.Image")));
+            this.redImage.InitialImage = null;
+            this.redImage.Location = new System.Drawing.Point(15, 331);
+            this.redImage.Name = "redImage";
+            this.redImage.Size = new System.Drawing.Size(49, 51);
+            this.redImage.TabIndex = 2;
+            this.redImage.TabStop = false;
+            // 
+            // blueImage
+            // 
+            this.blueImage.Image = ((System.Drawing.Image)(resources.GetObject("blueImage.Image")));
+            this.blueImage.InitialImage = null;
+            this.blueImage.Location = new System.Drawing.Point(15, 388);
+            this.blueImage.Name = "blueImage";
+            this.blueImage.Size = new System.Drawing.Size(49, 51);
+            this.blueImage.TabIndex = 3;
+            this.blueImage.TabStop = false;
+            // 
+            // greenImage
+            // 
+            this.greenImage.Image = ((System.Drawing.Image)(resources.GetObject("greenImage.Image")));
+            this.greenImage.InitialImage = null;
+            this.greenImage.Location = new System.Drawing.Point(388, 352);
+            this.greenImage.Name = "greenImage";
+            this.greenImage.Size = new System.Drawing.Size(49, 51);
+            this.greenImage.TabIndex = 4;
+            this.greenImage.TabStop = false;
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.Location = new System.Drawing.Point(70, 352);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(208, 15);
+            this.redLabel.TabIndex = 5;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.Location = new System.Drawing.Point(70, 405);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(208, 15);
+            this.blueLabel.TabIndex = 6;
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.Location = new System.Drawing.Point(443, 367);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(208, 15);
+            this.greenLabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.greenImage);
+            this.Controls.Add(this.blueImage);
+            this.Controls.Add(this.redImage);
+            this.Controls.Add(this.sceneImage);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Breach";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox sceneImage;
+        private System.Windows.Forms.PictureBox redImage;
+        private System.Windows.Forms.PictureBox blueImage;
+        private System.Windows.Forms.PictureBox greenImage;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label greenLabel;
+    }
+}
+
---
+++ BreachAdventureGame/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace BreachAdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+
+        SoundPlayer sceneSound;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0) {scene = 1;}
+                else if (scene == 1) {scene = 3;}
+                if (scene == 2)
+                {
+                    //70% of scene 7, 30% of scene 2
+                    Random randGen = new Random();
+                    int value = randGen.Next(1, 11);
+
+                    if (value <= 7) { scene = 5; }
+                    else { scene = 6; }
+                }
+                 else if (scene == 6) { scene = 9; }
+                 else if (scene == 3) {scene = 7;}
+                 else if (scene == 8) {scene = 11;}
+                 else if (scene == 9) {scene = 12;}
+                // else if (scene == 6) {scene = 0;}
+                // else if (scene == 7) { }
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0) {scene = 2;}
+                else if (scene == 1) {scene = 4;}
+                else if (scene == 2) {scene = 1;}
+                else if (scene == 6) {scene = 10;}
+                else if (scene == 3) {scene = 8;}
+                else if (scene == 8) {scene = 14;}
+                else if (scene == 9) {scene = 13;}
+               // else if (scene == 7) {scene = 99;}
+            }
+            else if (e.KeyCode == Keys.Space)  //green button press
+            {
+               // if (scene == 0) { scene = 2; }
+               // else if (scene == 1) { scene = 4; }
+              //  else if (scene == 2) { scene = 1; }
+              //  else if (scene == 6) { scene = 10; }
+              //  else if (scene == 3) { scene = 8; }
+              //  else if (scene == 8) { scene = 14; }
+              //  else if (scene == 9) { scene = 13; }
+                // else if (scene == 7) {scene = 99;}
+            }
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:  //start scene  
+                  //  sceneImage.BackgroundImage = Properties.Resources.deepBrush;
+                  //  sceneSound = new SoundPlayer(Properties.Resources.jungle);
+                  //  sceneSound.Play();
+
+                    outputLabel.Text = "You are a test subject in an underground facility. The facility is currently having a containment breach. You must escape!";
+                    redLabel.Text = "Left wing";
+                    blueLabel.Text = "Right wing";
+                    greenLabel.Text = "";
+                    break;
+                case 1:
+                  //  sceneImage.BackgroundImage = Properties.Resources.forestLake;
+                  //  sceneSound = new SoundPlayer(Properties.Resources.brook);
+                  //  sceneSound.Play();
+
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 2:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 3:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 5:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 6:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 7:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 8:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 9:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 10:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 11:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 12:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 13:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 14:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 15:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 16:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 18:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 19:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 20:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 21:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 99:
+                    outputLabel.Text = "";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+
+                    Refresh();
+
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
